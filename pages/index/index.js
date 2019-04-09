32e96f870d4f30b3dbeb1465a6960a23e5fcb784
hsm <hsm19950709@gmail.com>
2019-04-09 18:47:15 +0800

    优化显示

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,14 +4,56 @@ const app = getApp()
 
 Page({
   data: {
+    isBtnShow: false,
     url: '',
-    isTextNull:0
+    isTextNull: 0,
+    modalHidden: true,
+    imgArr: ['res/help.png'],
+  },
+  /**
+  * 显示弹窗
+  */
+  showPic: function () {
+    console.log("showPic");
+    this.setData({
+      modalHidden: false
+    })
+  },
+  /**
+   * 点击取消
+   */
+  modalCandel: function () {
+    // do something
+    this.setData({
+      modalHidden: true
+    })
+  },
+  previewImg: function (e) {
+    console.log(e.currentTarget.dataset.index);
+    var index = 0;
+    var imgArr = this.data.imgArr;
+    wx.previewImage({
+      current: imgArr[index], //当前图片地址
+      urls: imgArr, //所有要预览的图片的地址集合 数组形式
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+  /**
+   *  点击确认
+   */
+  modalConfirm: function () {
+    // do something
+    this.setData({
+      modalHidden: true
+    })
   },
   //事件处理函数
   toDecode: function() {
     var that = this;
     console.log(that.data.url)
-    if (that.data.url.indexOf('http')>-1) {
+    if (that.data.url.indexOf('http') > -1) {
 
       wx.navigateTo({
         url: '/pages/save/save?url=' + that.data.url
@@ -30,17 +72,25 @@ Page({
       isTextNull: 0
     })
   },
-  pasteUrl: function () {
+  pasteUrl: function() {
     var that = this;
     wx.getClipboardData({
       success(res) {
-        that.setData({
-          url: res.data,
-          isTextNull: 1
-        })
+        if (res.data != null && res.data != '') {
+          that.setData({
+            url: res.data,
+            isTextNull: 1
+          });
+        }else{
+          wx.showToast({
+            title: '剪切板为空！',
+            icon: 'none',
+            duration: 2000
+          })
+        }
       }
     });
-    
+
   },
   bindUrlValue: function(e) {
     var that = this;
@@ -49,7 +99,7 @@ Page({
       url: value,
       isTextNull: 1
     })
-    if(value==''){
+    if (value == '') {
       that.setData({
         isTextNull: 0
       })
@@ -82,5 +132,17 @@ Page({
       }
     })
   },
-  
+  //转发
+  onShareAppMessage: function(res) {
+    if (res.from === 'button') {
+
+    }
+    return {
+      title: '我发现了一个好用的斗音视频去水印工具',
+      path: '/pages/index/index',
+      success: function(res) {
+        console.log('成功', res)
+      }
+    }
+  }
 })
