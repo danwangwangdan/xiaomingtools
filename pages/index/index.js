056ca5be6d80689dede812a8f61d0b981eb2af08
hsm <hsm19950709@gmail.com>
2019-04-11 00:19:50 +0800

    去水印

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,146 +3,168 @@
 const app = getApp()
 
 Page({
-  data: {
-    isBtnShow: false,
-    url: '',
-    isTextNull: 0,
-    modalHidden: true,
-    imgArr: ['res/help.png'],
-  },
-  /**
-  * 显示弹窗
-  */
-  showPic: function () {
-    console.log("showPic");
-    this.setData({
-      modalHidden: false
-    })
-  },
-  /**
-   * 点击取消
-   */
-  modalCandel: function () {
-    // do something
-    this.setData({
-      modalHidden: true
-    })
-  },
-  previewImg: function (e) {
-    console.log(e.currentTarget.dataset.index);
-    var index = 0;
-    var imgArr = this.data.imgArr;
-    wx.previewImage({
-      current: imgArr[index], //当前图片地址
-      urls: imgArr, //所有要预览的图片的地址集合 数组形式
-      success: function (res) { },
-      fail: function (res) { },
-      complete: function (res) { },
-    })
-  },
-  /**
-   *  点击确认
-   */
-  modalConfirm: function () {
-    // do something
-    this.setData({
-      modalHidden: true
-    })
-  },
-  //事件处理函数
-  toDecode: function() {
-    var that = this;
-    console.log(that.data.url)
-    if (that.data.url.indexOf('http') > -1) {
-
-      wx.navigateTo({
-        url: '/pages/save/save?url=' + that.data.url
-      });
-    } else {
-      wx.showToast({
-        title: '要输入正确的链接呀！',
-        icon: 'none',
-        duration: 3000
-      })
-    }
-  },
-  bindUrlClear: function() {
-    this.setData({
-      url: '',
-      isTextNull: 0
-    })
-  },
-  pasteUrl: function() {
-    var that = this;
-    wx.getClipboardData({
-      success(res) {
-        if (res.data != null && res.data != '') {
-          that.setData({
-            url: res.data,
-            isTextNull: 1
-          });
-        }else{
-          wx.showToast({
-            title: '剪切板为空！',
-            icon: 'none',
-            duration: 2000
+      data: {
+        isBtnShow: false,
+        url: '',
+        status: '正在获取支持的平台...',
+        isTextNull: 0,
+        modalHidden: true,
+        bnrUrl: ["https://www.daliandaxue.cn/img/qun.png",
+          "https://www.daliandaxue.cn/img/dybg.png"]
+        },
+        /**
+         * 显示弹窗
+         */
+        showPic: function() {
+          console.log("showPic");
+          this.setData({
+            modalHidden: false
           })
-        }
-      }
-    });
-
-  },
-  bindUrlValue: function(e) {
-    var that = this;
-    var value = e.detail.value;
-    that.setData({
-      url: value,
-      isTextNull: 1
-    })
-    if (value == '') {
-      that.setData({
-        isTextNull: 0
-      })
-    }
-  },
-  onLoad: function() {
-
-  },
-  onShow: function() {
-    var that = this;
-    wx.getClipboardData({
-      success(res) {
-        var lastUrl = wx.getStorageSync('lastUrl');
-        if (res.data.indexOf('http') > -1 && lastUrl != res.data) {
-          console.log(res.data);
-          that.setData({
-            url: res.data,
-            isTextNull: 1
-          });
-          wx.setStorage({
-            key: 'lastUrl',
-            data: res.data
+        },
+        /**
+         * 点击取消
+         */
+        modalCandel: function() {
+          // do something
+          this.setData({
+            modalHidden: true
           })
-          wx.showToast({
-            title: '自动获取成功',
-            icon: 'success',
-            duration: 1500
-          });
-        }
-      }
-    })
-  },
-  //转发
-  onShareAppMessage: function(res) {
-    if (res.from === 'button') {
+        },
+        previewImg: function(e) {
+          console.log(e.currentTarget.dataset.index);
+          var index = 0;
+          var bnrUrl = this.data.bnrUrl;
+          wx.previewImage({
+            current: bnrUrl[index], //当前图片地址
+            urls: bnrUrl, //所有要预览的图片的地址集合 数组形式
+            success: function(res) {},
+            fail: function(res) {},
+            complete: function(res) {},
+          })
+        },
+        /**
+         *  点击确认
+         */
+        modalConfirm: function() {
+          // do something
+          this.setData({
+            modalHidden: true
+          })
+        },
+        //事件处理函数
+        toDecode: function() {
+          var that = this;
+          console.log(that.data.url)
+          if (that.data.url.indexOf('http') > -1) {
 
-    }
-    return {
-      title: '我发现了一个好用的视频去水印工具',
-      path: '/pages/index/index',
-      success: function(res) {
-        console.log('成功', res)
-      }
-    }
-  }
-})
+            wx.navigateTo({
+              url: '/pages/save/save?url=' + that.data.url
+            });
+          } else {
+            wx.showToast({
+              title: '要输入正确的链接呀！',
+              icon: 'none',
+              duration: 3000
+            })
+          }
+        },
+        bindUrlClear: function() {
+          this.setData({
+            url: '',
+            isTextNull: 0
+          })
+        },
+        pasteUrl: function() {
+          var that = this;
+          wx.getClipboardData({
+            success(res) {
+              if (res.data != null && res.data != '') {
+                that.setData({
+                  url: res.data,
+                  isTextNull: 1
+                });
+              } else {
+                wx.showToast({
+                  title: '剪切板为空！',
+                  icon: 'none',
+                  duration: 2000
+                })
+              }
+            }
+          });
+
+        },
+        bindUrlValue: function(e) {
+          var that = this;
+          var value = e.detail.value;
+          that.setData({
+            url: value,
+            isTextNull: 1
+          })
+          if (value == '') {
+            that.setData({
+              isTextNull: 0
+            })
+          }
+        },
+        onLoad: function() {
+
+        },
+        onShow: function() {
+          var that = this;
+          wx.request({
+            url: 'https://www.daliandaxue.cn/hishelp/common/dystatus',
+            method: 'GET',
+            success(res) {
+              console.log(res.data);
+              if (res.data != null && res.data.data != null) {
+                that.setData({
+                  status: res.data.data.noticeText == '' ? '抖音√ 快手√ 火山√ 微视√' : res.data.data.noticeText
+                })
+              }
+            },
+            fail() {
+              $stopWuxRefresher() //停止下拉刷新
+              wx.showToast({
+                title: '网络请求失败，请稍后重试！',
+                icon: 'none',
+                duration: 3000
+              })
+            }
+          });
+          wx.getClipboardData({
+            success(res) {
+              var lastUrl = wx.getStorageSync('lastUrl');
+              if (res.data.indexOf('http') > -1 && lastUrl != res.data) {
+                console.log(res.data);
+                that.setData({
+                  url: res.data,
+                  isTextNull: 1
+                });
+                wx.setStorage({
+                  key: 'lastUrl',
+                  data: res.data
+                })
+                wx.showToast({
+                  title: '自动获取成功',
+                  icon: 'success',
+                  duration: 1500
+                });
+              }
+            }
+          })
+        },
+        //转发
+        onShareAppMessage: function(res) {
+          if (res.from === 'button') {
+
+          }
+          return {
+            title: '我发现了一个好用的视频去水印工具',
+            path: '/pages/index/index',
+            success: function(res) {
+              console.log('成功', res)
+            }
+          }
+        }
+      })
--- pages/save/save.js
+++ pages/save/save.js
@@ -7,7 +7,21 @@ Page({
     realUrl: '',
     isSaveBtnLoad: false,
     isSaveBtnDis: false,
-    saveBtnText: '存至相册'
+    saveBtnText: '存至相册',
+    bnrUrl: ["https://www.daliandaxue.cn/img/qun.png",
+      "https://www.daliandaxue.cn/img/dybg.png"]
+  },
+  previewImg: function (e) {
+    console.log(e.currentTarget.dataset.index);
+    var index = 0;
+    var bnrUrl = this.data.bnrUrl;
+    wx.previewImage({
+      current: bnrUrl[index], //当前图片地址
+      urls: bnrUrl, //所有要预览的图片的地址集合 数组形式
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
   },
   toSave: function() {
     var that = this;
@@ -60,34 +74,40 @@ Page({
     })
   },
   onLoad: function(options) {
-
     var vedioUrl = options.url;
-  console.log("options.url:" + vedioUrl)
-  var that = this;
-  if (vedioUrl != '' && vedioUrl.indexOf('douyin') > -1) {
-    wx.showLoading({
-      icon: 'none',
-      title: '解析视频中...',
-    })
-    wx.request({
-      url: app.globalData.localApiUrl + 'douyin/getRealUrl?url=' + encodeURIComponent(vedioUrl),
-      method: 'GET',
-      success(res) {
-        console.log(res.data);
-        wx.hideLoading();
-        if (res.data != null && res.data.code == 0) {
-          var data = res.data;
-          if (data.data != null && data.data.url != '') {
-            that.setData({
-              realUrl: data.data.url,
-              isSaveShow: true,
-              isVedioShow: true
-            })
-            wx.showToast({
-              title: '解析成功，您可以直接存至相册了！',
-              icon: 'none',
-              duration: 3000
-            })
+    console.log("options.url:" + vedioUrl)
+    var that = this;
+    if (vedioUrl != '' && vedioUrl.indexOf('douyin') > -1) {
+      wx.showLoading({
+        icon: 'none',
+        title: '解析视频中...',
+      })
+      wx.request({
+        url: app.globalData.localApiUrl + 'douyin/getRealUrl?url=' + encodeURIComponent(vedioUrl),
+        method: 'GET',
+        success(res) {
+          console.log(res.data);
+          wx.hideLoading();
+          if (res.data != null && res.data.code == 0) {
+            var data = res.data;
+            if (data.data != null && data.data.url != '') {
+              that.setData({
+                realUrl: data.data.url,
+                isSaveShow: true,
+                isVedioShow: true
+              })
+              wx.showToast({
+                title: '解析成功，您可以直接存至相册了！',
+                icon: 'none',
+                duration: 3000
+              })
+            } else {
+              wx.showToast({
+                title: '解析失败，请检查链接或联系客服处理！',
+                icon: 'none',
+                duration: 3000
+              })
+            }
           } else {
             wx.showToast({
               title: '解析失败，请检查链接或联系客服处理！',
@@ -95,47 +115,85 @@ Page({
               duration: 3000
             })
           }
-        } else {
+        },
+        fail() {
+          $stopWuxRefresher() //停止下拉刷新
           wx.showToast({
-            title: '解析失败，请检查链接或联系客服处理！',
+            title: '网络请求失败，请稍后重试！',
             icon: 'none',
             duration: 3000
           })
         }
-      },
-      fail() {
-        $stopWuxRefresher() //停止下拉刷新
-        wx.showToast({
-          title: '网络请求失败，请稍后重试！',
-          icon: 'none',
-          duration: 3000
-        })
-      }
-    });
-  } else if (vedioUrl.indexOf('gifshow') > -1 || vedioUrl.indexOf('huoshan') > -1) {
-    wx.showLoading({
-      icon: 'none',
-      title: '解析视频中...',
-    })
-    wx.request({
-      url: app.globalData.localApiUrl + 'video/download?url=' + encodeURIComponent(vedioUrl),
-      method: 'GET',
-      success(res) {
-        console.log(res.data);
-        wx.hideLoading();
-        if (res.data != null && res.data.code == 0) {
-          var data = res.data;
-          if (data.data != null && data.data.url != '') {
-            that.setData({
-              realUrl: data.data.url,
-              isSaveShow: true,
-              isVedioShow: true
-            })
-            wx.showToast({
-              title: '解析成功，您可以直接存至相册了！',
-              icon: 'none',
-              duration: 3000
-            })
+      });
+    } else if (vedioUrl.indexOf('gifshow') > -1 || vedioUrl.indexOf('huoshan') > -1) {
+      wx.showLoading({
+        icon: 'none',
+        title: '解析视频中...',
+      })
+      wx.request({
+        url: 'https://mini.fccabc.com/kuaishoumini',
+        method: 'POST',
+        data: {
+          "sourceURL": vedioUrl
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        success(res) {
+          console.log(res.data);
+          if (res.data != null && res.data.status == 0) {
+            var data = res.data;
+            if (data.data != null && data.data.realDownloadURL != '') {
+              wx.request({
+                url: app.globalData.localApiUrl + 'video/download?url=' + encodeURIComponent(data.data.realDownloadURL),
+                method: 'GET',
+                success(res) {
+                  console.log(res.data);
+                  wx.hideLoading();
+                  if (res.data != null && res.data.code == 0) {
+                    var data = res.data;
+                    if (data.data != null && data.data.url != '') {
+                      that.setData({
+                        realUrl: data.data.url,
+                        isSaveShow: true,
+                        isVedioShow: true
+                      })
+                      wx.showToast({
+                        title: '解析成功，您可以直接存至相册了！',
+                        icon: 'none',
+                        duration: 3000
+                      })
+                    } else {
+                      wx.showToast({
+                        title: '解析失败，请检查链接或联系客服处理！',
+                        icon: 'none',
+                        duration: 3000
+                      })
+                    }
+                  } else {
+                    wx.showToast({
+                      title: '解析失败，请检查链接或联系客服处理！',
+                      icon: 'none',
+                      duration: 3000
+                    })
+                  }
+                },
+                fail() {
+                  $stopWuxRefresher() //停止下拉刷新
+                  wx.showToast({
+                    title: '网络请求失败，请稍后重试！',
+                    icon: 'none',
+                    duration: 3000
+                  })
+                }
+              });
+            } else {
+              wx.showToast({
+                title: '解析失败，请检查链接或联系客服处理！',
+                icon: 'none',
+                duration: 3000
+              })
+            }
           } else {
             wx.showToast({
               title: '解析失败，请检查链接或联系客服处理！',
@@ -143,42 +201,83 @@ Page({
               duration: 3000
             })
           }
-        } else {
+        },
+        fail() {
+          $stopWuxRefresher() //停止下拉刷新
           wx.showToast({
-            title: '解析失败，请检查链接或联系客服处理！',
+            title: '网络请求失败，请稍后重试！',
             icon: 'none',
             duration: 3000
           })
         }
-      },
-      fail() {
-        $stopWuxRefresher() //停止下拉刷新
-        wx.showToast({
-          title: '网络请求失败，请稍后重试！',
-          icon: 'none',
-          duration: 3000
-        })
-      }
-    });
-  } else {
-    wx.showToast({
-      title: '链接不见了！',
-      icon: 'none',
-      duration: 3000
-    })
-  }
-},
-//转发
-onShareAppMessage: function(res) {
-  if (res.from === 'button') {
+      });
+    } else if (vedioUrl.indexOf('gifshow') > -1 || vedioUrl.indexOf('huoshan') > -1) {
+      wx.showLoading({
+        icon: 'none',
+        title: '解析视频中...',
+      })
+      wx.request({
+        url: app.globalData.localApiUrl + 'vedio/weishi?url=' + encodeURIComponent(vedioUrl),
+        method: 'GET',
+        success(res) {
+          console.log(res.data);
+          wx.hideLoading();
+          if (res.data != null && res.data.code == 0) {
+            var data = res.data;
+            if (data.data != null && data.data.url != '') {
+              that.setData({
+                realUrl: data.data.url,
+                isSaveShow: true,
+                isVedioShow: true
+              })
+              wx.showToast({
+                title: '解析成功，您可以直接存至相册了！',
+                icon: 'none',
+                duration: 3000
+              })
+            } else {
+              wx.showToast({
+                title: '解析失败，请检查链接或联系客服处理！',
+                icon: 'none',
+                duration: 3000
+              })
+            }
+          } else {
+            wx.showToast({
+              title: '解析失败，请检查链接或联系客服处理！',
+              icon: 'none',
+              duration: 3000
+            })
+          }
+        },
+        fail() {
+          $stopWuxRefresher() //停止下拉刷新
+          wx.showToast({
+            title: '网络请求失败，请稍后重试！',
+            icon: 'none',
+            duration: 3000
+          })
+        }
+      });
+    } else {
+      wx.showToast({
+        title: '链接不见了！',
+        icon: 'none',
+        duration: 3000
+      })
+    }
+  },
+  //转发
+  onShareAppMessage: function(res) {
+    if (res.from === 'button') {
 
-  }
-  return {
-    title: '我发现了一个免费好用的短视频去水印工具',
-    path: '/pages/index/index',
-    success: function(res) {
-      console.log('成功', res)
+    }
+    return {
+      title: '我发现了一个免费好用的短视频去水印工具',
+      path: '/pages/index/index',
+      success: function(res) {
+        console.log('成功', res)
+      }
     }
   }
-}
 })
