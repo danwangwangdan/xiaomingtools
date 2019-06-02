c67eed35475cbbf2b9c86568cd286cda4f89feed
hsm <hsm19950709@gmail.com>
2019-06-02 20:59:12 +0800

    新增积分机制

--- pages/me/me.js
+++ pages/me/me.js
@@ -5,73 +5,98 @@ Page({
    * 页面的初始数据
    */
   data: {
-   nickname:'张三',
-   imgSrc: '',
-   point:100
-
+    nickname: '姓名',
+    imgSrc: '',
+    point: 0,
+    signCount: 0,
+    shareCount: 0,
+    videoCount: 0,
   },
-  toSign: function (options) {
+  toSign: function(options) {
     console.log("签到");
+    wx.request({
+      url: app.globalData.localApiUrl + 'douyin/getRealUrl?url=' + encodeURIComponent(vedioUrl),
+      method: 'GET',
+      success(res) {
+        console.log(res.data);
+        wx.hideLoading();
+        var data = res.data;
+        if (data.data != null && data.data.url != '') {
+          that.setData({
+            realUrl: data.data.url,
+            isSaveShow: true,
+            isVedioShow: true
+          })
+        }
+      },
+      fail() {
+        wx.showToast({
+          title: '网络请求失败，请稍后重试！',
+          icon: 'none',
+          duration: 3000
+        })
+      }
+    });
   },
-  toShare: function (options) {
+  toShare: function(options) {
     console.log("分享");
   },
-  toVideo: function (options) {
+  toVideo: function(options) {
     console.log("视频");
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- pages/save/save.js
+++ pages/save/save.js
@@ -49,115 +49,106 @@ Page({
     //   videoAd.onClose(res => {
     //     // 用户点击了【关闭广告】按钮
     //     if (res && res.isEnded) {
-          // 正常播放结束，可以下发游戏奖励
-          wx.showToast({
-            title: '开始存至相册，感谢支持！',
-            icon: 'none',
-            duration: 2000
-          })
-          var that = this;
-          wx.authorize({
-            scope: "scope.writePhotosAlbum"
-          })
-          that.setData({
-            isSaveBtnLoad: true,
-            isSaveBtnDis: true
-          })
-          const downloadTask = wx.downloadFile({
-            url: that.data.realUrl,
+    // 正常播放结束，可以下发游戏奖励
+    wx.showToast({
+      title: '开始存至相册，感谢支持！',
+      icon: 'none',
+      duration: 2000
+    })
+    var that = this;
+    wx.authorize({
+      scope: "scope.writePhotosAlbum"
+    })
+    that.setData({
+      isSaveBtnLoad: true,
+      isSaveBtnDis: true
+    })
+    const downloadTask = wx.downloadFile({
+      url: that.data.realUrl,
+      success(res) {
+        console.log("开始下载...")
+        console.log(res)
+        if (res.statusCode === 200) {
+          wx.saveVideoToPhotosAlbum({
+            filePath: res.tempFilePath,
             success(res) {
-              console.log("开始下载...")
               console.log(res)
-              if (res.statusCode === 200) {
-                wx.saveVideoToPhotosAlbum({
-                  filePath: res.tempFilePath,
-                  success(res) {
-                    console.log(res)
-                    wx.showToast({
-                      title: '保存成功，请去系统相册查看！',
-                      icon: 'none',
-                      duration: 3000
-                    })
-                  }
-                })
-
-              } 
-              else {
-                wx.showToast({
-                  title: '连接服务器失败，请联系客服！',
-                  icon: 'none',
-                  duration: 3000
-                })
-              }
-            }
-          });
-          downloadTask.onProgressUpdate((res) => {
-            console.log('下载进度', res)
-            that.setData({
-              saveBtnText: '存至相册中' + res.progress + '%...'
-            });
-            if (res.progress === 100) {
-              that.setData({
-                isSaveBtnLoad: false,
-                isSaveBtnDis: false,
-                saveBtnText: '存至相册'
+              wx.showToast({
+                title: '保存成功，请去系统相册查看！',
+                icon: 'none',
+                duration: 3000
               })
             }
+          })
+        } else {
+          wx.showToast({
+            title: '连接服务器失败，请联系客服！',
+            icon: 'none',
+            duration: 3000
           })
         }
-        //  else {
-        //   wx.showToast({
-        //     title: '请观看完广告再下载哦，谢谢支持！',
-        //     icon: 'none',
-        //     duration: 3000
-        //   })
-        // }
-    //   })
-    // }
-  },
-  onLoad: function(options) {
-    // if (wx.createRewardedVideoAd) {
-    //   videoAd = wx.createRewardedVideoAd({
-    //     adUnitId: 'adunit-498062c378a63ba4'
-    //   })
-    //   videoAd.onLoad(() => {})
-    //   videoAd.onError((err) => {})
-    //   videoAd.onClose((res) => {})
-    // }
-    var vedioUrl = options.url;
-    // console.log("options.url:" + vedioUrl)
-    var that = this;
-    if (vedioUrl != '' && vedioUrl.indexOf('douyin') > -1) {
-      wx.showLoading({
-        icon: 'none',
-        title: '解析视频中...',
-      })
-      wx.request({
-        url: app.globalData.localApiUrl + 'douyin/getRealUrl?url=' + encodeURIComponent(vedioUrl),
-        method: 'GET',
-        success(res) {
-          console.log(res.data);
-          wx.hideLoading();
-          if (res.data != null && res.data.code == 0) {
-            var data = res.data;
-            if (data.data != null && data.data.url != '') {
-              that.setData({
-                realUrl: data.data.url,
-                isSaveShow: true,
-                isVedioShow: true
-              })
-              wx.showToast({
-                title: '解析成功，您可以直接存至相册了！',
-                icon: 'none',
-                duration: 3000
-              })
-            } else {
-              wx.showToast({
-                title: '解析失败，请检查链接或联系客服处理！',
-                icon: 'none',
-                duration: 3000
-              })
-            }
+      }
+    });
+    downloadTask.onProgressUpdate((res) => {
+      console.log('下载进度', res)
+      that.setData({
+        saveBtnText: '存至相册中' + res.progress + '%...'
+      });
+      if (res.progress === 100) {
+        that.setData({
+          isSaveBtnLoad: false,
+          isSaveBtnDis: false,
+          saveBtnText: '存至相册'
+        })
+      }
+    })
+  // }
+  //  else {
+  //   wx.showToast({
+  //     title: '请观看完广告再下载哦，谢谢支持！',
+  //     icon: 'none',
+  //     duration: 3000
+  //   })
+  // }
+  //   })
+  // }
+},
+onLoad: function(options) {
+  // if (wx.createRewardedVideoAd) {
+  //   videoAd = wx.createRewardedVideoAd({
+  //     adUnitId: 'adunit-498062c378a63ba4'
+  //   })
+  //   videoAd.onLoad(() => {})
+  //   videoAd.onError((err) => {})
+  //   videoAd.onClose((res) => {})
+  // }
+  var vedioUrl = options.url;
+  // console.log("options.url:" + vedioUrl)
+  var that = this;
+  if (vedioUrl != '' && vedioUrl.indexOf('douyin') > -1) {
+    wx.showLoading({
+      icon: 'none',
+      title: '解析视频中...',
+    })
+    wx.request({
+      url: app.globalData.localApiUrl + 'douyin/getRealUrl?url=' + encodeURIComponent(vedioUrl),
+      method: 'GET',
+      success(res) {
+        console.log(res.data);
+        wx.hideLoading();
+        if (res.data != null && res.data.code == 0) {
+          var data = res.data;
+          if (data.data != null && data.data.url != '') {
+            that.setData({
+              realUrl: data.data.url,
+              isSaveShow: true,
+              isVedioShow: true
+            })
+            wx.showToast({
+              title: '解析成功，您可以直接存至相册了！',
+              icon: 'none',
+              duration: 3000
+            })
           } else {
             wx.showToast({
               title: '解析失败，请检查链接或联系客服处理！',
@@ -165,61 +156,61 @@ Page({
               duration: 3000
             })
           }
-        },
-        fail() {
-          $stopWuxRefresher() //停止下拉刷新
+        } else {
           wx.showToast({
-            title: '网络请求失败，请稍后重试！',
+            title: '解析失败，请检查链接或联系客服处理！',
             icon: 'none',
             duration: 3000
           })
         }
-      });
-    } else if (vedioUrl.indexOf('gifshow') > -1 || vedioUrl.indexOf('huoshan') > -1) {
-      wx.showLoading({
-        icon: 'none',
-        title: '解析视频中...',
-      })
-      wx.request({
-        url: 'https://mini.fccabc.com/kuaishoumini',
-        method: 'POST',
-        data: {
-          "sourceURL": vedioUrl
-        },
-        header: {
-          'content-type': 'application/json'
-        },
-        success(res) {
-          console.log(res.data);
-          if (res.data != null && res.data.status == 0) {
-            var data = res.data;
-            if (data.data != null && data.data.realDownloadURL != '') {
-              wx.request({
-                url: app.globalData.localApiUrl + 'video/download?url=' + encodeURIComponent(data.data.realDownloadURL),
-                method: 'GET',
-                success(res) {
-                  console.log(res.data);
-                  wx.hideLoading();
-                  if (res.data != null && res.data.code == 0) {
-                    var data = res.data;
-                    if (data.data != null && data.data.url != '') {
-                      that.setData({
-                        realUrl: data.data.url,
-                        isSaveShow: true,
-                        isVedioShow: true
-                      })
-                      wx.showToast({
-                        title: '解析成功，您可以直接存至相册了！',
-                        icon: 'none',
-                        duration: 3000
-                      })
-                    } else {
-                      wx.showToast({
-                        title: '解析失败，请检查链接或联系客服处理！',
-                        icon: 'none',
-                        duration: 3000
-                      })
-                    }
+      },
+      fail() {
+        $stopWuxRefresher() //停止下拉刷新
+        wx.showToast({
+          title: '网络请求失败，请稍后重试！',
+          icon: 'none',
+          duration: 3000
+        })
+      }
+    });
+  } else if (vedioUrl.indexOf('gifshow') > -1 || vedioUrl.indexOf('huoshan') > -1) {
+    wx.showLoading({
+      icon: 'none',
+      title: '解析视频中...',
+    })
+    wx.request({
+      url: 'https://mini.fccabc.com/kuaishoumini',
+      method: 'POST',
+      data: {
+        "sourceURL": vedioUrl
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        console.log(res.data);
+        if (res.data != null && res.data.status == 0) {
+          var data = res.data;
+          if (data.data != null && data.data.realDownloadURL != '') {
+            wx.request({
+              url: app.globalData.localApiUrl + 'video/download?url=' + encodeURIComponent(data.data.realDownloadURL),
+              method: 'GET',
+              success(res) {
+                console.log(res.data);
+                wx.hideLoading();
+                if (res.data != null && res.data.code == 0) {
+                  var data = res.data;
+                  if (data.data != null && data.data.url != '') {
+                    that.setData({
+                      realUrl: data.data.url,
+                      isSaveShow: true,
+                      isVedioShow: true
+                    })
+                    wx.showToast({
+                      title: '解析成功，您可以直接存至相册了！',
+                      icon: 'none',
+                      duration: 3000
+                    })
                   } else {
                     wx.showToast({
                       title: '解析失败，请检查链接或联系客服处理！',
@@ -227,23 +218,23 @@ Page({
                       duration: 3000
                     })
                   }
-                },
-                fail() {
-                  $stopWuxRefresher() //停止下拉刷新
+                } else {
                   wx.showToast({
-                    title: '网络请求失败，请稍后重试！',
+                    title: '解析失败，请检查链接或联系客服处理！',
                     icon: 'none',
                     duration: 3000
                   })
                 }
-              });
-            } else {
-              wx.showToast({
-                title: '解析失败，请检查链接或联系客服处理！',
-                icon: 'none',
-                duration: 3000
-              })
-            }
+              },
+              fail() {
+                $stopWuxRefresher() //停止下拉刷新
+                wx.showToast({
+                  title: '网络请求失败，请稍后重试！',
+                  icon: 'none',
+                  duration: 3000
+                })
+              }
+            });
           } else {
             wx.showToast({
               title: '解析失败，请检查链接或联系客服处理！',
@@ -251,86 +242,93 @@ Page({
               duration: 3000
             })
           }
-        },
-        fail() {
-          $stopWuxRefresher() //停止下拉刷新
+        } else {
           wx.showToast({
-            title: '网络请求失败，请稍后重试！',
+            title: '解析失败，请检查链接或联系客服处理！',
             icon: 'none',
             duration: 3000
           })
         }
-      });
-    } else if (decodeURIComponent(vedioUrl).indexOf('weishi') > -1) {
-      wx.showLoading({
-        icon: 'none',
-        title: '解析视频中...',
-      })
-      console.log(decodeURIComponent(vedioUrl).indexOf("&reqseq"));
-      vedioUrl = decodeURIComponent(vedioUrl).substring(0, decodeURIComponent(vedioUrl).indexOf("&reqseq"));
-      console.log("weishi：" + vedioUrl);
-      wx.request({
-        url: app.globalData.localApiUrl + 'video/weishi?url=' + encodeURIComponent(vedioUrl),
-        method: 'GET',
-        success(res) {
-          console.log(res.data);
-          wx.hideLoading();
-          if (res.data != null && res.data.code == 0) {
-            var data = res.data;
-            if (data.data != null && data.data.url != '') {
-              that.setData({
-                realUrl: data.data.url,
-                isSaveShow: true,
-                isVedioShow: true
-              })
-              wx.showToast({
-                title: '解析成功，您可以直接存至相册了！',
-                icon: 'none',
-                duration: 3000
-              })
-            } else {
-              wx.showToast({
-                title: '解析失败，请检查链接或联系客服处理！',
-                icon: 'none',
-                duration: 3000
-              })
-            }
-          } else {
-            wx.showToast({
-              title: '解析失败，请检查链接或联系客服处理！',
-              icon: 'none',
-              duration: 3000
-            })
-          }
-        },
-        fail() {
-          $stopWuxRefresher() //停止下拉刷新
-          wx.showToast({
-            title: '网络请求失败，请稍后重试！',
-            icon: 'none',
-            duration: 3000
-          })
-        }
-      });
-    } else {
-      wx.showToast({
-        title: '链接不见了！',
-        icon: 'none',
-        duration: 3000
-      })
-    }
-  },
-  //转发
-  onShareAppMessage: function(res) {
-    if (res.from === 'button') {
-
-    }
-    return {
-      title: '我发现了一个免费好用的短视频去水印工具',
-      path: '/pages/index/index',
-      success: function(res) {
-        console.log('成功', res)
+      },
+      fail() {
+        $stopWuxRefresher() //停止下拉刷新
+        wx.showToast({
+          title: '网络请求失败，请稍后重试！',
+          icon: 'none',
+          duration: 3000
+        })
       }
+    });
+  } else if (decodeURIComponent(vedioUrl).indexOf('weishi') > -1) {
+    wx.showLoading({
+      icon: 'none',
+      title: '解析视频中...',
+    })
+    console.log(decodeURIComponent(vedioUrl).indexOf("&reqseq"));
+    vedioUrl = decodeURIComponent(vedioUrl).substring(0, decodeURIComponent(vedioUrl).indexOf("&reqseq"));
+    console.log("weishi：" + vedioUrl);
+    wx.request({
+      url: app.globalData.localApiUrl + 'video/weishi?url=' + encodeURIComponent(vedioUrl),
+      method: 'GET',
+      success(res) {
+        console.log(res.data);
+        wx.hideLoading();
+        if (res.data != null && res.data.code == 0) {
+          var data = res.data;
+          if (data.data != null && data.data.url != '') {
+            that.setData({
+              realUrl: data.data.url,
+              isSaveShow: true,
+              isVedioShow: true
+            })
+            wx.showToast({
+              title: '解析成功，您可以直接存至相册了！',
+              icon: 'none',
+              duration: 3000
+            })
+          } else {
+            wx.showToast({
+              title: '解析失败，请检查链接或联系客服处理！',
+              icon: 'none',
+              duration: 3000
+            })
+          }
+        } else {
+          wx.showToast({
+            title: '解析失败，请检查链接或联系客服处理！',
+            icon: 'none',
+            duration: 3000
+          })
+        }
+      },
+      fail() {
+        $stopWuxRefresher() //停止下拉刷新
+        wx.showToast({
+          title: '网络请求失败，请稍后重试！',
+          icon: 'none',
+          duration: 3000
+        })
+      }
+    });
+  } else {
+    wx.showToast({
+      title: '链接不见了！',
+      icon: 'none',
+      duration: 3000
+    })
+  }
+},
+//转发
+onShareAppMessage: function(res) {
+  if (res.from === 'button') {
+
+  }
+  return {
+    title: '我发现了一个免费好用的短视频去水印工具',
+    path: '/pages/index/index',
+    success: function(res) {
+      console.log('成功', res)
     }
   }
+}
 })
